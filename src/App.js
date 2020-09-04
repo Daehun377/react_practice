a01b8fb08711c13e5ba40b2159599010b06b0d0e
Daehun Kim <gidonskaris@gmail.com>
2020-09-04 14:55:01 +0900

    [Error] 에러 나는중

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Movie from "./components/Movie";
+const movies = [
 
-function App() {
-    return(
-        <div>
-            <h1>방가방가</h1>
-        </div>
-    );
+    {
+        title: "Matrix",
+        poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
+    },
+    {
+        title: "Full Metal Jacket",
+        poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+    },
+    {
+        title: {},
+        poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
+    },
+    {
+        title: "Star Wars",
+        poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
+    }
+]
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className={"App"}>
+                {movies.map(movie => {
+                    return (
+                        <div>
+                            <Movie title = {movie.title}/>
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
 }
 
 
